55124487e8873d2f6efe41b7e8d31bd6a7ec729d
impptg <muhahadawang@icloud.com>
2022-10-11 22:24:56 +0800

    first commit

--- main.py
+++ main.py
@@ -5,13 +5,9 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 import schedule
+import config
 
 login_url = 'http://gba.bjtu.edu.cn/login?redirect=%2Factivity%2Flist'
-
-mail_server = 'smtp.qq.com'
-mail_from = '1493173244@qq.com'
-mail_pswd = ''
-mail_to = '21125166@bjtu.edu.cn'
 
 # 正文
 msg = MIMEText('害嗨嗨，有新的会议咯，快抢', 'plain', 'utf-8')
@@ -22,21 +18,27 @@ msg['Subject'] = Header('新会议', 'utf-8')
 cntPre = 0
 
 
-def sendMail():
+def sendMail(content="", subject=""):
+    if content == "":
+        content = '害嗨嗨，有新的会议咯，快抢'
+        subject = '新活动'
+
+    msg = MIMEText(content, 'plain', 'utf-8')
+    msg['From'] = Header('QLJBot')
+    msg['To'] = Header('21125166')
+    msg['Subject'] = Header(subject, 'utf-8')
+
     try:
         # 连接
-        smtpobj = smtplib.SMTP_SSL(mail_server)
-        smtpobj.connect(mail_server, 465)
-
+        smtpobj = smtplib.SMTP_SSL(config.mail_server)
+        smtpobj.connect(config.mail_server, config.mail_port)
         # 登录
-        smtpobj.login(mail_from, mail_pswd)
-
+        smtpobj.login(config.mail_from, config.mail_pswd)
         # 发送
-        smtpobj.sendmail(mail_from, mail_to, msg.as_string())
+        smtpobj.sendmail(config.mail_from, config.mail_to, msg.as_string())
         print("邮件发送成功")
 
     except smtplib.SMTPException:
-        print()
         print("无法发送邮件")
 
     finally:
@@ -51,34 +53,43 @@ def refreshCount():
     browser = webdriver.Chrome(chrome_options=option)
     browser.get(login_url)
 
+    # 账号密码
     userInfo = browser.find_elements_by_class_name('el-input__inner')
     sleep(1)
-    userInfo[0].send_keys("21125166")
+    userInfo[0].send_keys(config.login_user)
 
     sleep(1)
-    userInfo[1].send_keys("")
+    userInfo[1].send_keys(config.login_pswd)
 
+    # 登陆
     sleep(1)
     loginBtn = browser.find_element_by_class_name("el-button--primary")
     loginBtn.click()
 
+    # 最新活动数
     sleep(2)
     countInfo = browser.find_element_by_class_name('el-pagination__total').text
     countNum = string.atoi(''.join(x for x in countInfo if x.isdigit()))
     return countNum
 
 
-def job():
+def refreshJob():
     cntNow = refreshCount()
     if cntNow > cntPre:
         sendMail()
+
+
+def liveJob():
+    sendMail("服务正常运行", "服务状况")
 
 
 if __name__ == '__main__':
     # 获取初始 Cnt
     cntPre = refreshCount()
 
-    # 定义循环任务
-    schedule.every(5).seconds.do(job)
+    # 2 小时刷新一次
+    schedule.every(2).hour.do(refreshJob())
+    # 每天 12 点判断服务是不是挂了
+    schedule.every().day.at("12:00").do(liveJob())
     while True:
         schedule.run_pending()
