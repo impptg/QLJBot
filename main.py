94b7ef64493ccc3f89099cc0503270bbca6ac949
impptg <muhahadawang@icloud.com>
2022-10-12 15:14:43 +0800

    首次部署

--- main.py
+++ main.py
@@ -18,20 +18,10 @@ msg['Subject'] = Header('新会议', 'utf-8')
 cntPre = 0
 
 
-def sendMail(content="", subject=""):
-    if content == "":
-        content = '害嗨嗨，有新的会议咯，快抢'
-        subject = '新活动'
-
-    msg = MIMEText(content, 'plain', 'utf-8')
-    msg['From'] = Header('QLJBot')
-    msg['To'] = Header('21125166')
-    msg['Subject'] = Header(subject, 'utf-8')
-
+def sendMail():
     try:
         # 连接
-        smtpobj = smtplib.SMTP_SSL(config.mail_server)
-        smtpobj.connect(config.mail_server, config.mail_port)
+        smtpobj = smtplib.SMTP_SSL(config.mail_server,465)
         # 登录
         smtpobj.login(config.mail_from, config.mail_pswd)
         # 发送
@@ -49,6 +39,7 @@ def sendMail(content="", subject=""):
 def refreshCount():
     option = webdriver.ChromeOptions()
     option.add_argument('disable-infobars')
+    option.add_argument('--headless')
 
     browser = webdriver.Chrome(chrome_options=option)
     browser.get(login_url)
@@ -69,18 +60,21 @@ def refreshCount():
     # 最新活动数
     sleep(2)
     countInfo = browser.find_element_by_class_name('el-pagination__total').text
-    countNum = string.atoi(''.join(x for x in countInfo if x.isdigit()))
+    countNum = int(''.join(x for x in countInfo if x.isdigit()))
     return countNum
 
 
 def refreshJob():
+    print("refreshJob")
     cntNow = refreshCount()
+    cntPre = cntNow
     if cntNow > cntPre:
         sendMail()
 
 
 def liveJob():
-    sendMail("服务正常运行", "服务状况")
+    sendMail()
+    print("liveJob")
 
 
 if __name__ == '__main__':
@@ -88,8 +82,8 @@ if __name__ == '__main__':
     cntPre = refreshCount()
 
     # 2 小时刷新一次
-    schedule.every(2).hour.do(refreshJob())
+    schedule.every(1).hours.do(refreshJob)
     # 每天 12 点判断服务是不是挂了
-    schedule.every().day.at("12:00").do(liveJob())
+    schedule.every().day.at("12:00").do(liveJob)
     while True:
         schedule.run_pending()
